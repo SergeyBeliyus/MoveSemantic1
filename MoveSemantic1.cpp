da165734f914628dc0ec9cc66ebdfdb56c856af7
Sergey Belousov <sergeybeliyus@yandex.ru>
2024-02-21 10:44:47 +0500

    Inittial Commit

---
+++ MoveSemantic1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+template <typename T>
+void move_vectors(std::vector<T>&v1, std::vector<T>&v2) {
+	v2 = std::move(v1);
+}
+
+int main(int argc, char* argv[]) {
+	std::vector <std::string> one = { "test_string1", "test_string2" };
+	std::vector <std::string> two;
+
+	std::cout << "one: ";
+	for (std::string i : one) {
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+
+	move_vectors(one, two);
+
+	std::cout << "two: ";
+	for (std::string i : two) {
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+}
